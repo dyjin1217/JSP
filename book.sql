6d310d1064eab032e070da87754a08aa8dcf5df5
dyjin1217 <dyjin@kei.re.kr>
2019-08-25 23:40:17 +0900

    Update book.sql

--- book.sql
+++ book.sql
@@ -1,10 +1,17 @@
-
-drop table book;
 create table book(
 id number not null,
 title varchar2(50),
-author varchar2(20),
-price numberr default 0,
+author varchar(20),
+price number default 0,
 qty number default 0,
 primary key(id)
 );
+
+insert into book values (1,'자바','망켄',50000,10);
+insert into book values (2,'C','망켄',30000,20);
+insert into book values (3,'파이썬','망켄',10000,15);
+insert into book values (4,'R','망켄',20000,25);
+insert into book values (5,'JSP','망켄',60000,30);
+
+select * from book;
+commit;
